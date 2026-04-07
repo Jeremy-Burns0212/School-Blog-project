b1fa20696d74caae03dd6c698906aae474bece29
Jeremy Burns <jburns3222@students.cptc.edu>
2026-04-07 15:06:49 -0700

    Add Article and Readers model classes (#3)
    Introduced Article and Readers classes in School_Blog_project.Models. Article represents blog posts with metadata, while Readers models user credentials and roles, including writer and editor flags. These models support article management and user role handling in the application.

---
+++ School Blog project/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace School_Blog_project.Models
+{
+	public class Article
+	{
+		public int ArticleID { get; set; }
+
+		public required string Title { get; set; }
+
+		public string? Author { get; set; }
+
+		public DateTime DatePublished { get; set; }
+
+		public string? ArticleImagePath { get; set; }
+	}
+}

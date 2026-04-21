f47501d0706c0888a160f2495d42218f052d9322
Kenny-42 <lilkenny642@gmail.com>
2026-04-20 18:32:04 -0700

    Update ArticleSummary with new properties and categories
    Added required Title and Date properties. Introduced optional Description and ImageUrl fields. Changed Categories to a List<string> to support multiple categories and initialized it by default.

--- School Blog project/Models/ArticleSummary.cs
+++ School Blog project/Models/ArticleSummary.cs
@@ -2,6 +2,12 @@
 {
 	public class ArticleSummary
 	{
-		
+		public required string Title { get; set; }
+		public required DateTime Date { get; set; }
+		public string? Description { get; set; }
+		public string? ImageUrl { get; set; }
+
+		// Support multiple categories
+		public List<string> Categories { get; set; } = new();
 	}
 }
